98d6558e935120cee049756d0ffd09e11a7d52ae
Your Name <you@example.com>
2024-08-22 11:21:11 +0800

    test and script

---
+++ setup.sh
@@ -0,0 +1,51 @@
+dfx identity use default
+
+#######################################################################
+# For Linux & Mac
+#######################################################################
+
+echo " "
+echo "--------------------------------------------------"
+echo "Stopping the local network"
+dfx stop
+
+echo " "
+echo "--------------------------------------------------"
+echo "Starting the local network as a background process"
+dfx start --clean --background
+
+#######################################################################
+echo "--------------------------------------------------"
+echo "Building the wasm with wasi-sdk"
+icpp build-wasm --to-compile all
+# icpp build-wasm --to-compile mine
+
+#######################################################################
+echo " "
+echo "--------------------------------------------------"
+echo "Deploying the wasm to a canister on the local network"
+dfx deploy chess23
+
+#######################################################################
+echo " "
+echo "--------------------------------------------------"
+echo "Setting canister_mode to chat-principal"
+dfx canister call chess23 set_canister_mode chat-principal
+
+#######################################################################
+echo " "
+echo "--------------------------------------------------"
+echo "Checking health endpoint"
+dfx canister call chess23 health
+
+#######################################################################
+echo " "
+echo "--------------------------------------------------"
+echo "Initializing the canister configurations"
+python -m scripts.nft_init --network local --canister chess23 --nft-supply-cap 0 --nft-symbol "" --nft-name "" --nft-description ""
+
+#######################################################################
+echo " "
+echo "--------------------------------------------------"
+echo "Uploading the model & tokenizer"
+python -m scripts.upload --network local --canister chess23 --model models/model.bin --tokenizer models/tok512.bin
